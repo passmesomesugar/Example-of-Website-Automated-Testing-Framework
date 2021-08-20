4b6217c9c1e81879396e06821b8f6e63f70731f8
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-08-20 16:25:30 +0600

    ++ .properties reader, SignInTest works.

--- src/test/java/com/github/passmesomesugar/smoke/BasicTestConditions.java
+++ src/test/java/com/github/passmesomesugar/smoke/BasicTestConditions.java
@@ -1,15 +1,27 @@
 package com.github.passmesomesugar.smoke;
 
 import com.codeborne.selenide.Selenide;
+import com.github.passmesomesugar.services.PropertyDataReader;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class BasicTestConditions {
     public String BASEURL = "https://boardgamegeek.com";
     public int URL_CHECKS_TIMEOUT = 4000;
+    public String currentEnv = System.getProperty("env");
+    public String blank =
+            System.getProperty("user.dir").concat("\\").concat("\\src\\test\\resources\\blankpage.html");
+    /*
+     * Please set a valid username and password in a "prod-env.properties" file and put it in "\src\test\resources".
+     * File contents:
+     * user.login.name=some_real_username
+     * user.password=some_real_password
+     * */
+    public String userLogin = PropertyDataReader.getProperties(currentEnv).getProperty("user.login.name");
+    public String userPassword = PropertyDataReader.getProperties(currentEnv).getProperty("user.password");
 
     public void openBlank() {
-        Selenide.open();
+        Selenide.open(blank);
     }
 
     @BeforeTest
--- src/test/java/com/github/passmesomesugar/smoke/SignInTest.java
+++ src/test/java/com/github/passmesomesugar/smoke/SignInTest.java
@@ -1,23 +1,20 @@
 package com.github.passmesomesugar.smoke;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.Selenide;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Selenide.element;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SignInTest extends BasicTestConditions {
     @Test(groups = "this")
     public void openHomeAndSignIn() {
         open(BASEURL);
         element(Selectors.byText("Sign In")).click();
-        /*
-         * Please used valid username and password to run this test.
-         * */
-        element(Selectors.byCssSelector("input[name='username']")).setValue("~");
-        element(Selectors.byCssSelector("input[name='password']")).setValue("~");
+        element(Selectors.byCssSelector("input[name='username']")).setValue(userLogin);
+        element(Selectors.byCssSelector("input[name='password']")).setValue(userPassword);
         element(Selectors.byXpath("//button[text()=' Sign In ']")).click();
-        Selenide.sleep(5000);
+        element(Selectors.byText("Sign In")).shouldNotHave(Condition.exist);
+        sleep(5000);
     }
 }
