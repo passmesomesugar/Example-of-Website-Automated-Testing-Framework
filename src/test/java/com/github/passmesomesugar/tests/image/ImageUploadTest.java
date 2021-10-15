f3b6863e25ae8d10cd4f5f960068defed0ee07c2
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-10-15 09:52:34 +0600

    Image upload test refactor

--- src/test/java/com/github/passmesomesugar/tests/image/ImageUploadTest.java
+++ src/test/java/com/github/passmesomesugar/tests/image/ImageUploadTest.java
@@ -11,20 +11,16 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class ImageUploadTest extends CommonTestConditions {
 
-    @Test(groups = "this", description = "this")
+    @Test(groups = "this", description = "one can upload image")
     public void uploadImage() {
         logIn();
         element(Selectors.byXpath("//span[text()=' " + userLogin + " ']")).click();
         element(Selectors.byLinkText("Gallery")).click();
         element(Selectors.byLinkText("Upload Image")).click();
-        //element(Selectors.byXpath("//*[contains(text(),'Choose Images')]")).click();
-        sleep(5000);
         File file =
                 element(Selectors.byXpath("//*[contains(text(),'Choose Images')]//input"))
                         .uploadFile(new File("C:\\Users\\PC\\Desktop\\workdir\\Java\\Showcase Testing Framework\\src\\test\\resources\\img\\HD_empty_image.png"));
-        sleep(5000);
         element(Selectors.byXpath("//button[contains(.,'Upload all')]")).click();
-        sleep(7000);
         element(Selectors.byXpath("//span[contains(.,'Upload Complete. ')]")).shouldBe(Condition.exist);
     }
 }
