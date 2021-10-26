e5a288bc11b0113a11898d42fe76bf13ed6dff6b
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-10-26 14:37:41 +0600

    Edit interests test now works.

--- src/test/java/com/github/passmesomesugar/tests/account/EditInterests.java
+++ src/test/java/com/github/passmesomesugar/tests/account/EditInterests.java
@@ -8,7 +8,8 @@ import static com.codeborne.selenide.Selenide.element;
 import static com.codeborne.selenide.Selenide.sleep;
 
 public class EditInterests extends CommonTestConditions {
-    @Test(groups = "this", description = "Пользователь может редактировать интересы")
+
+    @Test(groups = "this", description = "one can edit interests")
     public void editInterests() {
         logIn();
         element(Selectors.byXpath("//span[text()=' " + userLogin + " ']")).click();
@@ -16,7 +17,12 @@ public class EditInterests extends CommonTestConditions {
         sleep(2000);
         element(Selectors.byLinkText("Edit Interests")).scrollIntoView(false);
         element(Selectors.byLinkText("Edit Interests")).click();
-        element(Selectors.byId("hobbies")).setValue("Dflsafljslfj");
+        element(Selectors.byId("hobbies")).setValue("collecting butterflies");
+        element(Selectors.byId("favoritebooks")).setValue("Gucci mane's autobiography");
+        element(Selectors.byId("favoritemovies")).setValue("Robert B. Weide movies");
+        element(Selectors.byId("favoritemusic")).setValue("Metallica");
+        element(Selectors.byId("favoritetvshows")).setValue("Curb your enthusiasm");
+        element(Selectors.byXpath("//input[@value='Submit'] ")).click();
         sleep(5000);
     }
 }
