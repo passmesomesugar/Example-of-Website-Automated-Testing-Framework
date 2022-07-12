cfa09c9ca70a8c526079ec8cbfff3ed04ec3c24b
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2022-07-12 19:51:15 +0600

    Facebook page test modified

--- src/test/java/com/github/passmesomesugar/page_object_pattern_tests/links_to_pages_tests/Facebook.java
+++ src/test/java/com/github/passmesomesugar/page_object_pattern_tests/links_to_pages_tests/Facebook.java
@@ -1,17 +1,21 @@
 package com.github.passmesomesugar.page_object_pattern_tests.links_to_pages_tests;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import com.github.passmesomesugar.CommonTestConditions;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selenide.element;
 import static com.codeborne.selenide.WebDriverConditions.urlContaining;
 
 public class Facebook extends CommonTestConditions {
 
-    @Test(groups = "this", description = "Accessing Facebook page")
+    @Test(groups = "this", description = "Accessing Facebook - BGG Group page")
     public void signInAndSignOut() {
         getIndexPage().openPage();
         getIndexPage().clickFacebook();
         Selenide.webdriver().shouldHave(urlContaining("www.facebook.com"));
+        element(Selectors.byXpath("//a[text()='BoardGameGeek']")).should(Condition.exist);
     }
 }
