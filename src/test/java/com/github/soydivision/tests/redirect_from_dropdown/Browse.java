8f25376554c8549ef3efe06bfced0bb7c586e4d7
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2022-11-13 19:07:44 +0600

    browse test, tested

--- src/test/java/com/github/soydivision/tests/redirect_from_dropdown/Browse.java
+++ src/test/java/com/github/soydivision/tests/redirect_from_dropdown/Browse.java
@@ -8,50 +8,139 @@ import org.testng.annotations.Test;
 import static com.codeborne.selenide.Selenide.element;
 
 public class Browse extends CommonTestConditions {
-    @Test(groups = {"sanity", "all"})
-    public void redirect_all_boardgames() {
+
+    @Test(groups = {"this", "all"})
+    public void accessIndexPage() {
         getIndexPage().openPage();
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseAllBoardgames() {
         getIndexPage().clickBrowseDropDown();
         getIndexPage().clickOnByText("All Boardgames");
         element(Selectors.byAttribute("class", "collection_table")).should(Condition.exist);
     }
 
-    @Test(groups = {"sanity", "all"})
-    public void redirect_categories() {
-        getIndexPage().openPage();
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseCategories() {
         getIndexPage().clickBrowseDropDown();
         getIndexPage().clickOnByText("Categories");
-        element(Selectors.byXpath("//h3[text()='Board Game Categories']")).should(Condition.exist);
+        element(Selectors.byText("Board Game Categories")).should(Condition.exist);
         element(Selectors.byAttribute("class", "forum_table")).should(Condition.exist);
     }
 
-    @Test(groups = {"sanity", "all"})
-    public void redirect_artists() {
-        getIndexPage().openPage();
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseArtists() {
         getIndexPage().clickBrowseDropDown();
         getIndexPage().clickOnByText("Artists");
-        element(Selectors.byXpath("//h3[text()='Board Game Artists']")).should(Condition.exist);
+        element(Selectors.byText("Board Game Artists")).should(Condition.exist);
         element(Selectors.byAttribute("class", "forum_table")).should(Condition.exist);
     }
 
-    @Test(groups = {"sanity", "all"})
-    public void redirect_publishers() {
-        getIndexPage().openPage();
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browsePublishers() {
         getIndexPage().clickBrowseDropDown();
         getIndexPage().clickOnByText("Publishers");
-        element(Selectors.byXpath("//h3[text()='Board Game Publishers']")).should(Condition.exist);
+        element(Selectors.byText("Board Game Publishers")).should(Condition.exist);
         element(Selectors.byAttribute("class", "forum_table")).should(Condition.exist);
     }
 
-//            Honors
-//    Gone Cardboard
-//    Recent Additions
-//    Previews
-//            Families
-//    Mechanics
-//            Designers
-//    Accessories
-//    Random Game
-//    Podcasts
-//  Wiki
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseHonors() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Honors");
+        element(Selectors.byText("Board Game Honors")).should(Condition.exist);
+        element(Selectors.byAttribute("class", "forum_table")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseGoneCardboard() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Gone Cardboard");
+        element(Selectors.byText("Gone Cardboard listings")).should(Condition.exist);
+        element(Selectors.byAttribute("id", "maincontent")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseRecentAdditions() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Recent Additions");
+        element(Selectors.byText("Recent Additions")).should(Condition.exist);
+        element(Selectors.byAttribute("class", "forum_table")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browsePreviews() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Recent Additions");
+        element(Selectors.byText("Recent Additions")).should(Condition.exist);
+        element(Selectors.byAttribute("class", "forum_table")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseFamilies() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Families");
+        element(Selectors.byText("Families")).should(Condition.exist);
+        element(Selectors.byAttribute("class", "forum_table")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseMechanics() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Mechanics");
+        element(Selectors.byText("Mechanics")).should(Condition.exist);
+        element(Selectors.byAttribute("id", "maincontent")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseDesigners() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Designers");
+        element(Selectors.byText("Board Game Designers")).should(Condition.exist);
+        element(Selectors.byAttribute("id", "maincontent")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseAccessories() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Accessories");
+        element(Selectors.byText("Accessory Rank")).should(Condition.exist);
+        element(Selectors.byAttribute("id", "maincontent")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseRandomGame() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Random Game");
+        element(Selectors.byText("Overview")).should(Condition.exist);
+        element(Selectors.byText("Description")).should(Condition.exist);
+        element(Selectors.byText("Ratings")).should(Condition.exist);
+        element(Selectors.byText("Forums")).should(Condition.exist);
+        element(Selectors.byText("Images")).should(Condition.exist);
+        element(Selectors.byText("Videos")).should(Condition.exist);
+        element(Selectors.byText("Files")).should(Condition.exist);
+        element(Selectors.byText("Stats")).should(Condition.exist);
+        element(Selectors.byText("Versions")).should(Condition.exist);
+        element(Selectors.byText("Expansions")).should(Condition.exist);
+        element(Selectors.byText("My Games")).should(Condition.exist);
+        element(Selectors.byText("Shopping")).should(Condition.exist);
+        element(Selectors.byText("More")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browsePodcasts() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Accessories");
+        element(Selectors.byText("Accessories")).should(Condition.exist);
+        element(Selectors.byAttribute("id", "maincontent")).should(Condition.exist);
+    }
+
+    @Test(groups = {"this", "all"}, priority = 1)
+    public void browseWiki() {
+        getIndexPage().clickBrowseDropDown();
+        getIndexPage().clickOnByText("Wiki");
+        element(Selectors.byText("Index")).should(Condition.exist);
+        element(Selectors.byAttribute("id", "maincontent")).should(Condition.exist);
+    }
 }
